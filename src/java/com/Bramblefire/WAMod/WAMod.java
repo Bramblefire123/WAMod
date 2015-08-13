261ce7b50c52bc4fb5cc8428d7012a3f27d77b00
Bramblefire <legomando@yahoo.com>
2015-08-13 00:23:09 -0500

    basic reference class implimented

--- src/java/com/Bramblefire/WAMod/WAMod.java
+++ src/java/com/Bramblefire/WAMod/WAMod.java
@@ -1,17 +1,18 @@
 package com.Bramblefire.WAMod;
 
 import com.Bramblefire.WAMod.proxy.IProxy;
+import com.Bramblefire.WAMod.reference.Reference;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = "WAMod", name = "WA Support Mod", version = "1.7.10-0.0.1")
+@Mod(modid = Reference.Mod_ID, name = Reference.Mod_NAME, version = Reference.VERSION)
 
 public class WAMod {
 
-    @Mod.Instance("WAMod")
+    @Mod.Instance(Reference.Mod_ID)
     public static WAMod instance;
 
     @SidedProxy(clientSide = "com.Bramblefire.WAMod.proxy.clientProxy", serverSide = "com.Bramblefire.WAMod.proxy.serverProxy")
--- src/java/com/Bramblefire/WAMod/reference/Reference.java
+++ src/java/com/Bramblefire/WAMod/reference/Reference.java
@@ -1,5 +1,9 @@
 package com.Bramblefire.WAMod.reference;
 
 
-public class Reference {
+public class Reference
+{
+    public static final String Mod_ID = "WAMod";
+    public static final String Mod_NAME = "WA Support Mod";
+    public static final String VERSION = "1.7.10-0.0.1";
 }

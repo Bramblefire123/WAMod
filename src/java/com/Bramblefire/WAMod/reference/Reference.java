7542f0e97bb7360dcfd651702962bd3b479da12f
Bramblefire <legomando@yahoo.com>
2015-08-17 22:01:00 -0500

    update to 1.8 as well as mod refocus

--- src/java/com/Bramblefire/WAMod/reference/Reference.java
+++ src/java/com/Bramblefire/WAMod/reference/Reference.java
@@ -4,6 +4,6 @@ package com.Bramblefire.WAMod.reference;
 public class Reference
 {
     public static final String Mod_ID = "WAMod";
-    public static final String Mod_NAME = "WA Support Mod";
-    public static final String VERSION = "1.7.10-0.0.1";
+    public static final String Mod_NAME = "WA Companion Mod";
+    public static final String VERSION = "1.8-0.0.1";
 }
